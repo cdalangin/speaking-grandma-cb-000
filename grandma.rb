3517f42bbb27cf91bca835df9a2723b223ade004
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-09-14 01:22:54 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,7 @@
 def speak_to_grandma(phrase)
   if "#{phrase}" == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  end
   if "#{phrase}.uppercase"
     return "NO, NOT SINCE 1938!"
   elsif "#{phrase}.lowercase"
