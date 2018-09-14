4a46313f48ab4f05701fd8d12e4713f1f84909fd
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-09-14 01:27:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(phrase)
   elsif "#{phrase}.uppercase"
     return "NO, NOT SINCE 1938!"
   end
-  if "#{phrase}.lowercase"
+  if "#{phrase}.!lowercase"
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
